1feca4eb7c15093459ea1a1e6d741ea37046f764
RollingRo <rohan.kathuria@live.com>
2025-05-11 01:37:28 -0700

    update readme

--- README.md
+++ README.md
@@ -3,13 +3,17 @@
 
 Read the paper [here!](https://papers.nips.cc/paper_files/paper/2012/hash/c399862d3b9d6b76c8436e924a68c45b-Abstract.html)
 
-A majority of the baseline code (including how to load images, etc.) was taken from [this](https://www.digitalocean.com/community/tutorials/alexnet-pytorch) article.
+A majority of the data processing code (how to load images, etc.) was taken from [this article](https://www.digitalocean.com/community/tutorials/alexnet-pytorch).
 
 ## Paper implementation overview
-My personal implementation of AlexNet is done in a combination of NumPy and PyTorch. I've implemented only pieces relevant to AlexNet myself in NumPy (as their own `nn.Module`), and the rest has been implemented via PyTorch.
+My implementation of AlexNet is done in a combination of NumPy and PyTorch. I've implemented only pieces relevant to AlexNet __exclusively__ in NumPy (as their own `nn.Module`), and the rest has been implemented via PyTorch (Linear layers, etc.)
 
-The implementation includes three key components:
+The NumPy implementation includes three key components:
 
 - **CNN**: A convolutional layer (similar to torch.nn.Conv2d)
 - **max_pooling**: A max pooling layer (similar to torch.nn.MaxPool2d)
 - **ReLU**: A ReLU activation function (like torch.nn.ReLU)
+
+You can view the implementation of each one in `layers.py` ([here](https://github.com/RollingRo11/dropout/blob/main/layers.py))
+
+Additionally, I've implemented `torch.nn.Dropout` in the multilayer perceptron (as also done by the authors of the paper) in order to reduce overfitting and boost weight learnings- (I've already implemented Dropout on its own, you can check it out [here!](https://github.com/RollingRo11/dropout))
--- model.py
+++ model.py
@@ -5,55 +5,39 @@ from layers import CNN, ReLU, max_pooling
 class AlexNet(nn.Module):
     def __init__(self, num_classes=10):
         super(AlexNet, self).__init__()
-        self.layer1 = nn.Sequential(
+        self.cnn_network = nn.Sequential(
             CNN(3, 96, kernel_size=11, stride=4, padding=0),
             nn.BatchNorm2d(96),
             ReLU(),
             max_pooling(kernel_size=3, stride=2),
-        )
-        self.layer2 = nn.Sequential(
             CNN(96, 256, kernel_size=5, stride=1, padding=2),
             nn.BatchNorm2d(256),
             ReLU(),
             max_pooling(kernel_size=3, stride=2),
-        )
-        self.layer3 = nn.Sequential(
             CNN(256, 384, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(384),
             ReLU(),
-        )
-        self.layer4 = nn.Sequential(
             CNN(384, 384, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(384),
             ReLU(),
-        )
-        self.layer5 = nn.Sequential(
             CNN(384, 256, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(256),
             ReLU(),
             max_pooling(kernel_size=3, stride=2),
         )
-        self.fc = nn.Sequential(nn.Dropout(0.5), nn.Linear(9216, 4096), ReLU())
-        self.fc1 = nn.Sequential(nn.Dropout(0.5), nn.Linear(4096, 4096), ReLU())
-        self.fc2 = nn.Sequential(nn.Linear(4096, num_classes))
+        self.mlp = (
+            nn.Sequential(
+                nn.Dropout(0.5),
+                nn.Linear(9216, 4096),
+                ReLU(),
+                nn.Dropout(0.5),
+                nn.Linear(4096, 4096),
+                ReLU(),
+                nn.Linear(4096, num_classes),
+            ),
+        )
 
     def forward(self, x):
-        out = self.layer1(x)
-        print("---layer 1 complete")
-        out = self.layer2(out)
-        print("---layer 2 complete")
-
-        out = self.layer3(out)
-        print("---layer 3 complete")
-        out = self.layer4(out)
-        print("---layer 4 complete")
-        out = self.layer5(out)
-        print("---layer 5 complete")
-        out = out.reshape(out.size(0), -1)
-        out = self.fc(out)
-        print("---layer fc0 complete")
-        out = self.fc1(out)
-        print("---layer fc1 complete")
-        out = self.fc2(out)
-        print("---layer fc2 complete")
+        out = self.cnn_network(x)
+        out = self.mlp(out)
         return out
